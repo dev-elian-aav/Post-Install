221a0627fdf949ac5cf67b5c97abb5a7bceae7fa
Elian AAV <dev_elian_aav@outlook.com>
2024-10-05 13:22:52 -0300

    Push some basic post-install goodies

---
+++ program-list.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Xorg minimal
+apt install xserver-xorg-core xserver-xorg-input-evdev xserver-xorg-video-intel xdg-user-dirs x11-xserver-utils x11-xkb-utils x11-utils xinit
+
+# Fonts
+
+# Terminal Programs
+apt install doas neovim calcurse flatpak pulseaudio pulsemixer xwallpaper
+
+# GUI Programs
+apt install keepassxc firefox-esr thunderbird vlc zathura zathura-djvu zathura-pdf-poppler zathura-ps
