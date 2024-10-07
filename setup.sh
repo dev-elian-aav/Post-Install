f293941ca213083d9427c6b80fab3e62ddeafc08
Elian Adolfo Argañaraz Velazco <elian@programming.elian.xyz>
2024-10-07 09:39:53 -0300

    Previous version works, but half of the commands had to be written manually

--- setup.sh
+++ setup.sh
@@ -18,20 +18,28 @@ apt install make clang libx11-dev libxft-dev libxinerama-dev git -y
 apt install keepassxc firefox-esr thunderbird vlc zathura zathura-djvu zathura-pdf-poppler zathura-ps blueman -y
 
 # Flatpak Setup
-su - elian
+sudo -u "elian" bash << flatconfiguration
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak update
 flatpak install io.freetubeapp.FreeTube
+flatconfiguration
 
-# Final Procedures
+# Folders and Git
+sudo -u "elian" bash << foldergitclones
 xdg-user-dirs-update
 git clone "https://github.com/dev-elian-aav/Suckless" "$hdir/Public/Suckless"
 git clone "https://github.com/dev-elian-aav/Post-Install" "$hdir/Public/Post-Install"
 git clone "https://github.com/dev-elian-aav/C---Kernighan-and-Ritchie" "$hdir/Public/C - Kernighan and Ritchie"
+wget "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts" -O $HOME/Downloads/hosts
+foldergitclones
 
-# Etc files
-wget "https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling-porn-social/hosts" $HOME/Downloads/hosts
-su - root
+# Dotfiles
+sudo -u "elian" bash << dotfiles
+cp $hdir/Public/Post-Install/Dot-files/xinitrc $hdir/.xinitrc
+cp $hdir/Public/Post-Install/Dot-files/profile $hdir/.profile
+dotfiles
+
+#Return to Root
 mv $hdir/Downloads/hosts /etc/
 cp $hdir/Public/Post-Install/doas.conf /etc/
 
@@ -39,8 +47,3 @@ cp $hdir/Public/Post-Install/doas.conf /etc/
 make clean install -C $hdir/Public/Suckless/dwm-6.5/
 make clean install -C $hdir/Public/Suckless/dmenu-5.3/
 make clean install -C $hdir/Public/Suckless/st-0.9.2/
-
-# Dot files
-su - elian
-cp $hdir/Public/Post-Install/Dot-files/xinitrc $hdir/.xinitrc
-cp $hdir/Public/Post-Install/Dot-files/profile $hdir/.profile
